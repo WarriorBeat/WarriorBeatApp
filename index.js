c886459e758290ab801eb93fc47440df4dfd94f1
Braden Mars <bradenmars02@gmail.com>
2018-11-03 22:10:02 -0600

    Appcenter Pre Build, Amplify config
    * Appcenter Prebuild script installs and pulls awsmobile project when building on appcenter
    * Cleaned up and fixed some problems with Amplify config

--- index.js
+++ index.js
@@ -6,29 +6,12 @@ import Amplify from "aws-amplify"
 import config from "./aws-exports"
 import "es6-symbol/implement"
 
-// Local API for local dev
-Amplify.configure({
-  ...config,
-  ...{
-    API: {
-      endpoints: [
-        {
-          name: "local",
-          endpoint: "http://localhost:5000"
-        },
-        {
-          name: "warriorbeat-stage",
-          endpoint:
-            "https://m6vkw9r8ud.execute-api.us-east-1.amazonaws.com/stage"
-        },
-        {
-          name: "warriorbeat-dev",
-          endpoint: "https://ps05owvrph.execute-api.us-east-1.amazonaws.com/dev"
-        }
-      ]
-    }
-  }
+// Local Testing Api
+config.aws_cloud_logic_custom.push({
+  name: "local",
+  endpoint: "http://localhost:5000"
 })
+Amplify.configure(config)
 
 registerScreens()
 
