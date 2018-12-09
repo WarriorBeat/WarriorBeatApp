ef1076f53814fbce17bdf6d5dead64e27327dbee
Braden Mars <bradenmars02@gmail.com>
2018-12-09 04:03:54 -0600

    refactor(api): Update API to comply with v0.6.0
    Refactored fetch methods to take advantage of resource relational includes

--- app/api/api.js
+++ app/api/api.js
@@ -4,7 +4,6 @@
  *  api
  */
 
-import * as log from "loglevel"
 import { API as REQ } from "aws-amplify"
 import { API_DEV } from "react-native-dotenv"
 
@@ -17,18 +16,16 @@ const gateways = {
 
 class API {
   resource = null
-  constructor(resource) {
+  constructor(resource, identity) {
     this.resource_type = resource
-    log.info("API DEV", API_DEV)
+    this.identity = identity
   }
 
   get gateway() {
     switch (API_DEV) {
     case "local":
-      log.info("CONNECTED TO LOCALHOST")
       return gateways.local
     case "awsdev":
-      log.info("CONNECTED TO AWS DEV")
       return gateways.aws_dev
     default:
       return gateways.aws
@@ -40,12 +37,32 @@ class API {
     return path
   }
 
+  /**
+   * Fetch all items at resource endpoint
+   *
+   * @returns - All Items at resource endpoint
+   * @memberof API
+   */
   async fetchAll() {
     const resp = await REQ.get(this.gateway, this.resource)
     return resp
   }
+
+  async fetchIDs() {
+    const resp = await REQ.get(this.gateway, this.resource)
+    let ids = resp.map(item => {
+      return item[this.identity]
+    })
+    return ids
+  }
+
+  async get(id, includes) {
+    let url = `${this.resource}/${id}?include=${includes.join(",")}`
+    const resp = await REQ.get(this.gateway, url)
+    return resp
+  }
 }
 
-export const PostAPI = new API("posts")
-export const CategoryAPI = new API("categories")
-export const AuthorAPI = new API("authors")
+export const PostAPI = new API("posts", "postId")
+export const CategoryAPI = new API("categories", "categoryId")
+export const AuthorAPI = new API("authors", "authorId")
