c398097604ebe6d1b723c7ad261c45c3dad133b4
Braden Mars <bradenmars02@gmail.com>
2018-12-16 19:52:54 -0600

    feat(polls): Implemented PollStore
    Added new PollStore to RootStore and Provider

---
+++ app/stores/pollStore.js
@@ -0,0 +1,104 @@
+/**
+ *  pollStore.js
+ *  mobx store for poll data
+ *  stores
+ */
+
+import {
+  observable, flow, computed, reaction, when,
+} from "mobx"
+
+export class Poll {
+  id = null
+
+  autoSave = false
+
+  store = null
+
+  saveHandler = null
+
+  constructor(store, id) {
+    this.store = store
+    this.id = id
+    this.saveHandler = reaction(
+      () => this.asJson,
+      (json) => {
+        if (this.autoSave) {
+          this.store.updatePoll(json)
+        }
+      },
+    )
+  }
+
+  @computed
+  get asJson() {
+    return {
+      pollId: this.id,
+      question: this.question,
+      status: this.status,
+      date: this.date,
+      answers: this.answers,
+      total_votes: this.total_votes,
+    }
+  }
+
+  parsePollDate = (isoDate) => {
+    const date = new Date(isoDate)
+    const parsed = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`
+    return parsed
+  }
+
+  updateFromJson(json) {
+    this.autoSave = false
+    this.id = json.pollId
+    this.question = json.question
+    this.status = json.status
+    this.date = this.parsePollDate(json.date)
+    this.answers = json.answers
+    this.total_votes = json.total_votes
+    this.autoSave = true
+  }
+}
+
+export class PollStore {
+  resourceClient
+
+  @observable
+  polls = []
+
+  @observable
+  state = "pending"
+
+  constructor(rootStore, resourceClient) {
+    this.rootStore = rootStore
+    this.resourceClient = resourceClient
+    this.loadPolls()
+  }
+
+  loadPolls = flow(function* () {
+    this.state = "pending"
+    this.polls = []
+    const polls = yield this.resourceClient.fetchAll()
+    when(
+      () => this.rootStore.authorStore.status === "ready",
+      () => {
+        polls.forEach(json => this.updatePoll(json))
+        this.state = "ready"
+      },
+    )
+  })
+
+  updatePoll(json) {
+    let poll = this.polls.find(p => p.id === json.pollId)
+    if (!poll) {
+      poll = new Poll(this, json.pollId)
+      this.polls.push(poll)
+    }
+    poll.updateFromJson(json)
+  }
+
+  resolvePoll(id) {
+    const poll = this.polls.find(p => p.id === id)
+    return poll !== null ? poll : null
+  }
+}
--- app/stores/rootStore.js
+++ app/stores/rootStore.js
@@ -6,7 +6,7 @@
 
 import API from "api"
 import {
-  PostStore, AuthorStore, MediaStore, CategoryStore,
+  PostStore, AuthorStore, MediaStore, CategoryStore, PollStore,
 } from "."
 
 export default class RootStore {
@@ -15,10 +15,12 @@ export default class RootStore {
     this.authorResource = new API("authors", "categoryId")
     this.categoryResource = new API("categories", "categoryId")
     this.postResource = new API("posts", "postId")
+    this.pollResource = new API("polls", "pollId")
 
     this.mediaStore = new MediaStore(this, this.mediaResource)
     this.categoryStore = new CategoryStore(this, this.categoryResource)
     this.authorStore = new AuthorStore(this, this.authorResource)
     this.postStore = new PostStore(this, this.postResource)
+    this.pollStore = new PollStore(this, this.pollResource)
   }
 }
