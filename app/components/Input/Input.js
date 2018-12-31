41d41e27f75d584974582b3b01f5288002d584e3
Braden Mars <BradenM@users.noreply.github.com>
2018-12-30 21:44:40 -0600

    feat: New Input Component
    Input component that wraps React Native element Input

---
+++ app/components/Input/Input.js
@@ -0,0 +1,33 @@
+/**
+ * Input.js
+ * Text Input Component
+ * components
+ */
+
+import React from "react"
+import { Input as REInput } from "react-native-elements"
+import Text from "components/Text"
+import styles from "./styles"
+
+const Input = (props) => {
+  const { label, hideText } = props
+  const textLabel = (
+    <Text Color="black_light" Type="header">
+      {label}
+    </Text>
+  )
+  return (
+    <REInput
+      containerStyle={styles.container}
+      inputContainerStyle={styles.inputContainer}
+      inputStyle={styles.input}
+      underlineColorAndroid="transparent"
+      {...props}
+      label={textLabel}
+      clearButtonMode="always"
+      secureTextEntry={!!hideText}
+    />
+  )
+}
+
+export default Input
---
+++ app/components/Input/index.js
@@ -0,0 +1,9 @@
+/**
+ * Input/index.js
+ * Index Exports for Input Component
+ * components
+ */
+
+import Input from "./Input"
+
+export default Input
