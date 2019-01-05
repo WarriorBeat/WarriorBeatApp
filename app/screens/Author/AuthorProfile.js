38e36bb321f5a0a26e5b3f54bb915d992855267e
Braden Mars <BradenM@users.noreply.github.com>
2019-01-04 23:58:41 -0600

    feat(Author): Added Header with Animated Background via ScrollHandler
    Added Header with background animated via comparing the scoll position against the mount position of
    a view inside the AuthorProfile

--- app/screens/Author/AuthorProfile.js
+++ app/screens/Author/AuthorProfile.js
@@ -5,25 +5,73 @@
  */
 
 import React from "react"
-import { View, ScrollView } from "react-native"
+import {
+  View, ScrollView, Animated, Easing, findNodeHandle,
+} from "react-native"
 import Text from "components/Text"
 import AuthorInfo from "components/Author/AuthorInfo"
 import { observer, inject, PropTypes as MobxTypes } from "mobx-react/native"
 import { observable } from "mobx"
-import { icons, colors } from "config/styles"
+import { icons, colors, rgba } from "config/styles"
 import { AuthorSubscribe, AuthorStats, AuthorPosts } from "components/Author"
+import { Header } from "components/Header"
 import styles from "./styles"
 
 class AuthorProfile extends React.Component {
-  componentDidMount() {}
+  @observable
+  headerActive = false
+
+  componentWillMount() {
+    this.animateHeader = new Animated.Value(0)
+  }
+
+  componentDidMount() {
+    setTimeout(
+      () => this._contentView.measureLayout(findNodeHandle(this._scrollView), (x, y) => {
+        this.contentOffset = y
+      }),
+      0,
+    )
+  }
+
+  handleScroll = (event) => {
+    const currentOffset = event.nativeEvent.contentOffset.y
+    const isOver = currentOffset >= this.contentOffset
+    if (this.headerActive !== isOver) {
+      this.headerActive = isOver
+      Animated.timing(this.animateHeader, {
+        toValue: this.headerActive ? 150 : 0,
+        duration: 500,
+        easing: Easing.cubic,
+      }).start()
+    }
+  }
 
   render() {
     const { author } = this.props
+    const headerBackground = this.animateHeader.interpolate({
+      inputRange: [0, 150],
+      outputRange: ["rgba(0,0,0,0.0)", rgba(colors.primary, 1)],
+    })
+    const AnimatedHeader = Animated.createAnimatedComponent(Header)
     return (
-      <View style={styles.root}>
-        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.rootContent}>
+      <Animated.View style={styles.root}>
+        <AnimatedHeader active={this.headerActive} backgroundColor={headerBackground} />
+        <ScrollView
+          ref={scrollView => (this._scrollView = scrollView)}
+          onScroll={e => this.handleScroll(e)}
+          scrollEventThrottle={1}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.rootContent}
+        >
           <AuthorInfo author={author} />
-          <View style={styles.subContainer}>
+          <View
+            onLayout={() => {}}
+            renderToHardwareTextureAndroid
+            collapsable={false}
+            ref={view => (this._contentView = view)}
+            style={styles.subContainer}
+          >
             <AuthorSubscribe containerStyle={styles.subButtonContainer} author={author} />
           </View>
           <View style={styles.bioContainer}>
@@ -39,7 +87,7 @@ class AuthorProfile extends React.Component {
             <AuthorPosts containerStyle={styles.authorPosts} author={author} />
           </View>
         </ScrollView>
-      </View>
+      </Animated.View>
     )
   }
 }
