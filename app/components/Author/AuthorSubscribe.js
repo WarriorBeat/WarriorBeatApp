bd9a1e7f6b65717e0476be980ac97c2da7c907eb
Braden Mars <BradenM@users.noreply.github.com>
2019-01-06 03:39:20 -0600

    feat(AuthorSubscribe): Added Subscription Handler to AuthorSubscribe

--- app/components/Author/AuthorSubscribe.js
+++ app/components/Author/AuthorSubscribe.js
@@ -7,27 +7,34 @@
 import React from "react"
 import { View } from "react-native"
 import { Button } from "react-native-elements"
-import { observer, PropTypes as MobxTypes } from "mobx-react/native"
+import { observer, inject, PropTypes as MobxTypes } from "mobx-react/native"
 import { subscribeStyles as styles, subscribeButton } from "./styles"
 
 /**
  * @todo Add Subscribe Functionality to AuthorSubscribe Component
  * @body When Author subscription functionality is ready, add onPress handler here
  */
-const AuthorSubscribe = props => (
-  <View style={styles.root}>
-    <Button
-      buttonStyle={styles.button}
-      iconContainerStyle={styles.iconContainer}
-      icon={subscribeButton.default}
-      raised
-      {...props}
-    />
-  </View>
+const AuthorSubscribe = inject("userStore")(
+  observer(({ userStore, author, ...props }) => {
+    const { normal, subscribed } = subscribeButton
+    const icon = userStore.user.isSubbed(author.id) ? subscribed : normal
+    return (
+      <View style={styles.root}>
+        <Button
+          buttonStyle={styles.button}
+          iconContainerStyle={styles.iconContainer}
+          icon={icon}
+          raised
+          onPress={() => userStore.toggleSubscribe(author.id)}
+          {...props}
+        />
+      </View>
+    )
+  }),
 )
 
 AuthorSubscribe.propTypes = {
   author: MobxTypes.observableObject.isRequired,
 }
 
-export default observer(AuthorSubscribe)
+export default AuthorSubscribe
