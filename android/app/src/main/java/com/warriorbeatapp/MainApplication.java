a5381fa3bb59d3643258939c152942a91d2820fe
Braden Mars <BradenM@users.noreply.github.com>
2019-01-12 16:40:54 -0500

    build: Implemented React-Native-Config, Linked Android

--- android/app/src/main/java/com/warriorbeatapp/MainApplication.java
+++ android/app/src/main/java/com/warriorbeatapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.warriorbeatapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import io.sentry.RNSentryPackage;
@@ -48,6 +49,7 @@ public class MainApplication extends NavigationApplication {
   protected List<ReactPackage> getPackages() {
     // Additional Packages
     return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new RNDeviceInfo(),
             new FastImageViewPackage(),
             new RNSentryPackage(),
