a566095ef3fcaeaa9312cd0a33a3f43dbad4b964
Braden Mars <BradenM@users.noreply.github.com>
2019-01-13 14:07:17 -0600

    build(fastlane): Replace echo with printf in load_secret

--- fastlane/load_secret.sh
+++ fastlane/load_secret.sh
@@ -15,12 +15,12 @@ done
 CONFIG="${@: -1}"
 
 if [[ $CLEAN = 1 ]]; then
-    echo "Removing $1 App Secret from: $CONFIG"
+    printf "Removing $1 App Secret from: $CONFIG \n"
     echo
     sed -i 's/'$SECRET'/APP_SECRET\$/g' $CONFIG
     grep --color=always -z "APP_SECRET" $CONFIG
 else
-    echo "Inserting $1 App Secret into: $CONFIG \n"
+    printf "Inserting $1 App Secret into: $CONFIG \n"
     echo
     sed -i 's/APP_SECRET\$/'$SECRET'/g' $CONFIG
     grep --color=always -z $SECRET $CONFIG
