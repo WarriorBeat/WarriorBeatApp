c66736fc956e96a835d55e510e558fe88ecdb1ce
Braden Mars <bradenmars02@gmail.com>
2019-03-24 01:49:15 -0500

    fix(api): Updated to new GraphQL API Design

--- app/graphql/mutations.js
+++ app/graphql/mutations.js
@@ -10,9 +10,9 @@ import { FetchPolls } from "./queries"
 
 // Vote Poll
 const votePollOption = gql`
-  mutation insertPollVote($option_id: ID!) {
-    insertPollVote(option_id: $option_id) {
-      option_id
+  mutation pollAddVote($optionId: ID!) {
+    pollAddVote(optionId: $optionId) {
+      optionId
     }
   }
 `
@@ -25,27 +25,27 @@ const mutations = {
           errorPolicy: "ignore",
         },
         variables: {
-          option_id: optionId,
+          optionId,
         },
         optimisticResponse: {
           __typename: "Mutation",
-          insertPollVote: { option_id: optionId, __typename: "PollOption" },
+          pollAddVote: { optionId, __typename: "PollOption" },
         },
-        update: (proxy, { data: { insertPollVote } }) => {
+        update: (proxy, { data: { pollAddVote } }) => {
           const data = proxy.readQuery({ query: FetchPolls })
-          const votedPoll = data.listPolls.items.find((p) => {
+          const votedPoll = data.pollList.items.find((p) => {
             const options = p.options.map(opt => opt.id)
-            if (options.includes(insertPollVote.option_id)) {
+            if (options.includes(pollAddVote.optionId)) {
               return p
             }
             return false
           })
           const optionIndex = votedPoll.options.findIndex(
-            opt => opt.id === insertPollVote.option_id,
+            opt => opt.id === pollAddVote.optionId,
           )
-          const pollIndex = data.listPolls.items.findIndex(p => p.id === votedPoll.id)
+          const pollIndex = data.pollList.items.findIndex(p => p.id === votedPoll.id)
           votedPoll.options[optionIndex].votes += 1
-          data.listPolls.items[pollIndex] = votedPoll
+          data.pollList.items[pollIndex] = votedPoll
           proxy.writeQuery({ query: FetchPolls, data })
         },
       }),
