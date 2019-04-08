a5174e66dc4e28bc1cfd9135df0c8d58f24577e1
Braden Mars <BradenM@users.noreply.github.com>
2019-04-07 22:02:42 -0500

    fix: Remove Orphaned FeedView Screen

--- app/screens/index.js
+++ app/screens/index.js
@@ -6,6 +6,5 @@
 
 import Home from "./Home/home"
 import Initializing from "./initializing"
-import FeedView from "./Feed/FeedView"
 
-export { Home, Initializing, FeedView }
+export { Home, Initializing }
