8c7b3617eec2b74269a20b4976f296fd73d895ce
Braden Mars <BradenM@users.noreply.github.com>
2019-04-12 23:32:18 -0500

    fix: Removed Deprecated Rest API Client Module

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 import Amplify from "aws-amplify"
 import { Navigation } from "react-native-navigation"
 import registerScreens from "config/screens"
-import API from "config/aws"
 import { Sentry } from "react-native-sentry"
 import awsExports from "./aws-exports"
 
@@ -15,7 +14,7 @@ if (!__DEV__) {
   Sentry.config("https://5dab653ac82e4d01ab0b08a1fbabd1c0@sentry.io/1358160").install()
 }
 // Set API Configuration
-Amplify.configure({ ...awsExports, API })
+Amplify.configure(awsExports)
 
 // Register RN Nav Screens
 registerScreens()
