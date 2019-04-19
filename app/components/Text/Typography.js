6b695721657206bedef716f309fddc64641a14ad
Braden Mars <BradenM@users.noreply.github.com>
2019-04-19 03:17:58 -0500

    feat(meta): Added About Us Screen (#84)
    * test(layout): Added Tests for Layout Components
    
    * feat(layout): Update Col in Grid, Added Flex and Justify Customizatio...
    
    * feat(text): Added HTMLBody Typographpy
    
    * test(text): Added Tests for Typography
    
    * feat: Register and Add About Us Screen Definitions
    
    * feat(uiStore): Added Preload method to prefetch Meta Data
    
    * feat: Added About Us Page

--- app/components/Text/Typography.js
+++ app/components/Text/Typography.js
@@ -6,7 +6,19 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import HTML from "react-native-render-html"
+import { StyleSheet } from "react-native"
+import { Colors } from "components/styles"
 import Text from "./Text"
+import * as TextStyle from "./styles"
+
+const styles = StyleSheet.create({
+  html: {
+    ...TextStyle.type.body,
+    ...Colors.color("blackPrimary"),
+    fontSize: 16,
+  },
+})
 
 export const Header = ({ children, Color }) => (
   <Text Weight="semibold" Color={Color}>
@@ -19,6 +31,11 @@ export const Body = ({ Color, children }) => (
     {children}
   </Text>
 )
+
+export const HTMLBody = (props) => {
+  const { content } = props
+  return <HTML html={content} baseFontStyle={styles.html} />
+}
 
 Header.propTypes = {
   children: PropTypes.node.isRequired,
@@ -36,4 +53,8 @@ Body.propTypes = {
 
 Body.defaultProps = {
   Color: "blackPrimary",
+}
+
+HTMLBody.propTypes = {
+  content: HTML.propTypes.html.isRequired,
 }
