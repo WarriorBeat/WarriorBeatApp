6a7e0a62317005db0c81945bed0909bb9cd9b58f
Braden Mars <BradenM@users.noreply.github.com>
2019-04-21 00:10:58 -0500

    feat: Added Shape.rect, Cleaned MeetTheStaff and added PropTypes

--- app/screens/Meta/MeetTheStaff.js
+++ app/screens/Meta/MeetTheStaff.js
@@ -6,13 +6,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { inject, observer } from "mobx-react/native"
+import { observer } from "mobx-react/native"
 import { PageWithHeader, Row, Col } from "components/Layout"
 import { AuthorCard } from "components/Author"
 import { compose } from "react-apollo"
-import { queries } from "graphql"
+import { queries, PropTypes as gqlTypes } from "graphql"
 
-@inject("uiStore")
 @observer
 class MeetTheStaff extends React.Component {
   render() {
@@ -20,13 +19,20 @@ class MeetTheStaff extends React.Component {
     return (
       <PageWithHeader title="Meet the Staff">
         <Col>
-          <Row width="100%" wrap>
-            {loading ? null : authors.map(a => <AuthorCard author={a} />)}
-          </Row>
+          <Row wrap>{loading ? null : authors.map(a => <AuthorCard author={a} />)}</Row>
         </Col>
       </PageWithHeader>
     )
   }
+}
+
+MeetTheStaff.propTypes = {
+  loading: PropTypes.bool,
+  authors: PropTypes.arrayOf(gqlTypes.author).isRequired,
+}
+
+MeetTheStaff.defaultProps = {
+  loading: true,
 }
 
 export default compose(queries.author.authorList)(MeetTheStaff)
