f9c5f6d757b9a8ef223797e31eb4f01f0d97d2e3
Braden Mars <BradenM@users.noreply.github.com>
2019-04-24 01:06:07 -0500

    feat(nav): Added Social Icons to NavMenu (#88)
    * feat(nav): Added Social Icons to NavMenu
    
    * test: Updated Snapshots, Added Social Tests

--- app/components/Generic/Social.js
+++ app/components/Generic/Social.js
@@ -8,24 +8,50 @@ import React from "react"
 import PropTypes from "prop-types"
 import { SocialIcon } from "react-native-elements"
 import { Config, Util } from "config"
+import { Colors, Position } from "components/styles"
 
-export const SocialButton = ({ type, url, onPress }) => (
-  <SocialIcon raised type={type} onPress={() => onPress(url)} />
+const getProps = (light, hPad) => {
+  if (!light) return {}
+  return {
+    raised: false,
+    iconStyle: {
+      ...Colors.bgColor("primaryDark"),
+      ...Colors.color("whiteSecondary"),
+      ...Position.hPadding(hPad),
+    },
+    light: true,
+    iconSize: 42,
+  }
+}
+
+export const SocialButton = ({
+  type, url, onPress, light, hPad,
+}) => (
+  <SocialIcon
+    type={type}
+    onPress={() => onPress(url)}
+    iconStyle={Position.hPadding(hPad)}
+    {...getProps(light, hPad)}
+  />
 )
 
-const Social = ({ onPress }) => {
+const Social = (props) => {
   const socials = Object.entries(Config.social)
-  return socials.map(s => <SocialButton key={s[0]} type={s[0]} url={s[1].url} onPress={onPress} />)
+  return socials.map(s => <SocialButton key={s[0]} type={s[0]} url={s[1].url} {...props} />)
 }
 
 SocialButton.propTypes = {
   type: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   onPress: PropTypes.func,
+  light: PropTypes.bool,
+  hPad: PropTypes.string,
 }
 
 SocialButton.defaultProps = {
   onPress: url => Util.openLink(url),
+  light: false,
+  hPad: "0%",
 }
 
 export default Social
